cbba976cbb4505debdd5c2cb97aeda9f701f48c9
Arnab Chatterjee <arnabc1701@gmail.com>
2021-05-23 11:27:14 +0530

    gallery added

---
+++ src/components/endbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Endbar extends Component {
+    render() {
+        return (
+            <div>
+                <div id="endbar" className="text-center">
+                    <div className="container" >
+                        <div className="section-title" >
+                            <h2>TKS MEDIA AGENCY</h2>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -43,6 +43,11 @@ export const Navigation = (props) => {
               </a>
             </li>
             <li>
+              <a href='#portfolio' className='page-scroll'>
+                Gallery
+              </a>
+            </li>
+            <li>
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
